4d0d0b60bfadc6cae0548759f6fce6fe075fde7d
AlicjaNowakowska <59330489+AlicjaNowakowska@users.noreply.github.com>
2021-09-21 12:26:27 +0200

    Update GUIForNetworkAnalysis.py

--- GUIForNetworkAnalysis.py
+++ GUIForNetworkAnalysis.py
@@ -26,41 +26,6 @@ import ipywidgets
 from IPython.display import HTML, display
 from DownloadButton import *
 
-class DownloadButton(ipywidgets.Button):
-    """Download button with dynamic content
-
-    The content is generated using a callback when the button is clicked.
-    """
-
-    def __init__(self, filename: str, contents: Callable[[], str], **kwargs):
-        super(DownloadButton, self).__init__(**kwargs)
-        self.filename = filename
-        self.contents = contents
-        self.on_click(self.__on_click)
-
-    def __on_click(self, b):
-        contents: bytes = self.contents().encode('utf-8')
-        b64 = base64.b64encode(contents)
-        payload = b64.decode()
-        digest = hashlib.md5(contents).hexdigest()  # bypass browser cache
-        id = f'dl_{digest}'
-
-        display(HTML(f"""
-<html>
-<body>
-<a id="{id}" download="{self.filename}" href="data:text/csv;base64,{payload}" download>
-</a>
-
-<script>
-(function download() {{
-document.getElementById('{id}').click();
-}})()
-</script>
-
-</body>
-</html>
-"""))
-
 # GUI:
 class GUI_for_graph_analysis:
   def __init__(self):
